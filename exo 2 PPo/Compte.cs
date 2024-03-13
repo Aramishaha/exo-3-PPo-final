38a702435a3c973619e71c97b2faf4480a58b610
MOHAMED ARAMIS Aramis <amohamed@edouardgand.fr>
2024-03-13 16:14:00 +0100

    Ajoutez des fichiers projet.

---
+++ exo 2 PPo/Compte.cs
@@ -0,0 +1,55 @@
+﻿namespace exo_2_PPo
+{
+    class Compte 
+    {
+        private int numeroCompte;
+        private string nomCompte;
+        private string prenomCompte;
+        private int soldeCompte;
+
+        public Compte(int monNumero, string monNom, string monPrenom) 
+        {
+            numeroCompte = monNumero;
+            nomCompte = monNom;
+            prenomCompte = monPrenom;
+            soldeCompte = 0;
+        }
+        public Compte(int monNumero, string monNom, string monPrenom, int monSolde) 
+        {
+            numeroCompte = monNumero;
+            nomCompte = monNom;
+            prenomCompte = monPrenom;
+            soldeCompte = monSolde;
+        }
+
+        public void CrediterCompte(int montant_Ajoute)
+        {
+            soldeCompte = soldeCompte + montant_Ajoute;
+
+        }
+        public void DebiterCompte(int montant_Soustrait)
+        {
+
+            soldeCompte = soldeCompte - montant_Soustrait;
+
+        }
+
+        public int GetNumero()
+        {
+            return numeroCompte;
+        }
+        public string GetNom()
+        {
+            return nomCompte;
+        }
+        public string GetPrenom()
+        {
+            return prenomCompte;
+        }
+        public int GetSolde()
+        {
+
+            return soldeCompte;
+        }
+    }
+}
---
+++ exo 2 PPo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo_2_PPo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Compte c1 = new Compte(1, "Aramis", "Mohamed Aramis", 0);
+            c1.CrediterCompte(2500);
+            c1.DebiterCompte(1000);
+            Console.WriteLine("Numero : " + c1.GetNumero());
+            Console.WriteLine("Nom : " + c1.GetNom());
+            Console.WriteLine("Prenom : " + c1.GetPrenom());
+            Console.WriteLine("Solde : " + c1.GetSolde());
+
+            Console.ReadKey();
+        }
+        
+
+    }
+}
